35296780ba158fbfefaa0759d8d6db89158a32dc
wiaqen0 <wia.wia.164@gmail.com>
2023-11-03 10:08:39 +0700

    rename models file

--- client/src/app/models.ts
+++ client/src/app/models.ts
@@ -1,15 +1,15 @@
 export class Product {
   productTitle: string;
   productImgSource: string;
-  productPrice: number|string;
-  constructor(productTitle: string = 'Hạt giống rau muống INDOFA',productImgSource: string ='test.png',productPrice: number|string=10000) {
+  productPrice: number | string;
+  constructor(productTitle: string = 'Hạt giống rau muống INDOFA', productImgSource: string = 'test.png', productPrice: number | string = 10000) {
     this.productTitle = productTitle;
     this.productImgSource = productImgSource;
     this.productPrice = this.numberWithCommas(productPrice);
   }
-  numberWithCommas(x:number|string) {
+  numberWithCommas(x: number | string) {
     var parts = x.toString().split(",");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return parts.join(".");
-}
+  }
 }
--- client/src/app/product-category-container/product-category-container.component.ts
+++ client/src/app/product-category-container/product-category-container.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Product } from '../product';
+import { Product } from '../models';
 @Component({
   selector: 'app-product-category-container',
   templateUrl: './product-category-container.component.html',
