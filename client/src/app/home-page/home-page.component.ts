4970d62b4f0c20e43b2eeb4183c48cdcde9460b5
nganguyen19 <nganvh1911@gmail.com>
2023-11-28 22:07:42 +0700

    signinup

--- client/src/app/home-page/home-page.component.ts
+++ client/src/app/home-page/home-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Emitters } from '../emitters/emitter';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
-
+export class HomePageComponent implements OnInit {
+message:string
+constructor(private http:HttpClient){}
+ngOnInit():void{
+  this.http
+  .get('http://localhost:5000/api/user',{withCredentials: true})
+  .subscribe(
+    (res:any)=> {
+    this.message=`Hi ${res.name}` 
+    Emitters.authEmitter.emit(true)
+  },
+  (err) => {
+    this.message=" You are not logged in"
+    Emitters.authEmitter.emit(false)
+  }
+  )
+}
 }
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,62 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  form: FormGroup
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ){
+
+  }
+  ngOnInit():void{
+    this.form= this.formBuilder.group({
+      name:'',
+      email:'',
+      password:'',
+      cfpassword:''
+    })
+  }
+  ValidateEmail=(email:any) =>
+{
+  var validRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+ 
+  if (email.match(validRegex))
+  {
+    return (true)
+  }
+   else{return (false)}
+    
+}
+  submit(): void{
+    let user = this.form.getRawValue()
+    console.log(user)
+    
+    if(user.name=='' || user.email=='' || user.password=='' || user.cfpassword==''){
+      alert("Please enter all the fields")
+    }
+    else if(user.password !== user.cfpassword){
+      alert("Password and confirm password do not match");
+      return;
+    }
+    else if (!this.ValidateEmail(user.email)){
+      alert("You have entered an invalid email address!")
+    }else{
+      this.http.post("http://localhost:5000/api/register",user,{
+        withCredentials:true
+      })
+      .subscribe(()=> this.router.navigate(['/']),(err)=>{
+        alert(err.error.message)
+      })
+    }
+  }
+}
