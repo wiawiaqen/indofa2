9199536e4f1ad7681d4e14a419abb59413b4029e
tmeiii <maipnt21406c@st.uel.edu.vn>
2023-12-20 14:36:45 +0700

    .

--- server/routes/coupon.js
+++ server/routes/coupon.js
@@ -4,7 +4,7 @@ const authMiddleware = require("../middlewares/auth");
 const couponService = require("../controllers/couponController");
 
 router.get("/", couponService.getAll);
-router.get("/:id", couponService.getOne);
+router.get("one/:id", couponService.getOne);
 router.get("/filter", couponService.filter);
 router.post("/one", authMiddleware.auth, authMiddleware.admin, couponService.createOne);
 router.post("/many", authMiddleware.auth, authMiddleware.admin, couponService.createMany);
--- server/routes/product.js
+++ server/routes/product.js
@@ -3,26 +3,15 @@ const router = Router();
 const authMiddleware = require("../middlewares/auth");
 const productService = require("../controllers/productController");
 
-router.get("/", async (req, res) => {
-  try {
-    const result = await Product.find({});
-    res.send(result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
 router.get("/", productService.getAll);
 router.get("/one/:id", productService.getOne);
-router.get("/filter", productService.filter);
 router.get("/pagination/:page", productService.pagination);
 router.get("/maxpage", productService.getMaxPage);
+router.get("/filter", productService.filter);
 router.post("/one", authMiddleware.auth, authMiddleware.admin, productService.createOne);
-router.post("/many", productService.createMany);
+router.post("/many", authMiddleware.auth, authMiddleware.admin, productService.createMany);
 router.put("/:id", authMiddleware.auth, authMiddleware.admin, productService.updateOne);
 router.delete("/:id", authMiddleware.auth, authMiddleware.admin, productService.deleteOne);
-
 module.exports = router;
 
 
